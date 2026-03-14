6fa21c6c99b9474b1f6454a7119b57878fd9275a
Deivid-Sena-DSC <deivid.sena.dsc@gmail.com>
2026-03-14 16:55:02 +0000

    ATUALIZAÇÃO DE TRATAMENTO DO TEXTO E VISUALIZAÇÃO

--- src/index.js
+++ src/index.js
@@ -11,19 +11,26 @@ const caminho_txt = diretorio_principal + "/arquivos"
 /* OBTENDO A LISTA DE ARQUIVOS NO DIRETÓRIO */
 const arquivos_do_diretorio = fs.readdirSync(caminho_txt)   
 
+/* FAZENDO UM FOR PARA VERIFICAR SE A QUANTIDADE DE ARQUIVOS É MAIOR QUE 0 */
 for (let i = 0; i < arquivos_do_diretorio.length; i++) {
+
+    /* VERIFICANDO SE O ARQUIVO TERMINA COM .TXT */
     if (arquivos_do_diretorio[i].endsWith('.txt')) {
+
+        /* ABRINDO O ARQUIVO */
         arquivo_txt = fs.readFileSync(caminho_txt + "/" + arquivos_do_diretorio[i], 'UTF-8')}}
 
-
+/* DIVIDINDO O CONTEÚDO DO ARQUIVO EM UM VETOR DE PALAVRAS */
 arquivo_txt = arquivo_txt.split(' ')
 
-console.log(arquivo_txt)
 
-
+/* VARIÁVEL PARA CONTAR A QUANTIDADE DE VEZES QUE A PALAVRA "CLIENTE" APARECE NO ARQUIVO */
 var palavras = 0;
 
+/* FAZENDO UM FOR PARA VERIFICAR CADA PALAVRA DO ARQUIVO */
 for (let i = 0; i < arquivo_txt.length; i++) {
+
+    /* TRANSFORMANDO AS PALAVRAS EM MAIÚSCULAS PARA VER SE É IGUAL A "CLIENTE" */
     arquivo_txt[i] = arquivo_txt[i].toUpperCase()
     if (arquivo_txt[i] == 'CLIENTE') {
         palavras++
@@ -31,20 +38,31 @@ for (let i = 0; i < arquivo_txt.length; i++) {
     
 }
 
+/* TRANSFORMANDO O VETOR DE PALAVRAS EM UMA STRING PARA PODER ESCREVER NO ARQUIVO */
 arquivo_txt = arquivo_txt.join(' ')
 
+
+/* ESCREVENDO O CONTEÚDO MODIFICADO NO ARQUIVO */
 fs.writeFileSync("arquivo_modificado.txt", arquivo_txt, 'UTF-8')
 
-console.log(palavras + '  ' + arquivo_txt)
+console.log("A palavra 'CLIENTE' apareceu " + palavras + " vezes no arquivo.")
 
+/* IMPORTANDO O MODULO EXPRESS PARA CRIAR UM SERVIDOR WEB */
 const express = require('express')
+
+/* CRIANDO O SERVIDOR WEB */
 const app = express()
+
+/* DEFININDO A PORTA DO SERVIDOR */
 const port = 3000
 
+/* DEFININDO A ROTA PRINCIPAL DO SERVIDOR */
 app.get('/', (req, res) => {
-    res.send(palavras.toString() + ' ' + ' ' + arquivo_txt)
+    /* ENVIANDO A QUANTIDADE DE VEZES QUE A PALAVRA "CLIENTE" APARECEU NO ARQUIVO E O CONTEÚDO DO ARQUIVO MODIFICADO PARA O NAVEGADOR */
+    res.send(palavras.toString() + '\n' + '\n' + arquivo_txt)
 })
 
+/* INICIANDO O SERVIDOR */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- src/index2.js
+++ src/index2.js
@@ -1,7 +1,33 @@
+/* BLIBLIOTECA FILE SYSTEM */
 const fs = require('fs')
 
+/* PEGA OS CAMINHOS DOS ARQUIVOS */
 const caminho_arquivo = process.argv;
+
+/* PEGA O LINK DO ARQUIVO */
 const link = caminho_arquivo[2];
+
+
+/* VERIFICA AS PALAVRAS DUPLICADAS */
+function VerificarPalvrasDuplicadas(texto) {
+    const resultado = {};
+
+    texto.forEach(palavra => {
+      resultado[palavra] = (resultado[palavra] || 0) + 1;
+      
+    });
+    return resultado;
+}
+
+/* AJUSTA O TEXTO PARA REMOVER CARACTERES ESPECIAIS */
+function ajustarTexto(texto) {
+    const texto_ajustado = texto.map(palavra => palavra.toTitleCase().replace(/[\(\)]/g, ' ').replace(/[\n\r]+/g, ' ').replace(/\s+/g, ' ').trim());
+    return texto_ajustado;
+}
+
+/* SEPARA O TEXTO EM PALAVRAS */
+const texto_ajustado = ajustarTexto(fs.readFileSync(link, 'utf-8').split(' '));
+
 
 
 const express = require('express')
@@ -9,7 +35,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send(fs.readFileSync(link, 'utf-8'));
+    res.send(fs.readFileSync(link, 'utf-8') + '----' + JSON.stringify(VerificarPalvrasDuplicadas(texto_ajustado)));
 });
 
 app.listen(port, () => {
