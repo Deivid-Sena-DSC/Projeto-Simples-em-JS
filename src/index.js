5c135fdcb4320bb4fe7474d8824ee31c809888b0
Deivid-Sena-DSC <deivid.sena.dsc@gmail.com>
2026-03-21 14:18:41 +0000

    ADICIONANDO A CONTAGEM DE PALAVRAS E EXIBINDO ORGANIZADO

--- src/index.js
+++ src/index.js
@@ -1,54 +1,42 @@
+const fs = require('fs');
 
-/* IMPORTANDO O MODULO FILE SYSTEM */
-const fs = require('fs')
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
 
-/* OBTENDO O DIRETÓRIO PRINCIPAL */
-const diretorio_principal = process.cwd(__dirname)
+fs.readFile(link, 'utf-8', (erro, texto) =>{
+    quebraEmParagrafos(texto);
+});
 
-/* OBTENDO O CAMINHO DOS ARQUIVOS */
-const caminho_txt = diretorio_principal + "/arquivos"
-
-/* OBTENDO A LISTA DE ARQUIVOS NO DIRETÓRIO */
-const arquivos_do_diretorio = fs.readdirSync(caminho_txt)   
-
-/* FAZENDO UM FOR PARA VERIFICAR SE A QUANTIDADE DE ARQUIVOS É MAIOR QUE 0 */
-for (let i = 0; i < arquivos_do_diretorio.length; i++) {
-
-    /* VERIFICANDO SE O ARQUIVO TERMINA COM .TXT */
-    if (arquivos_do_diretorio[i].endsWith('.txt')) {
-
-        /* ABRINDO O ARQUIVO */
-        arquivo_txt = fs.readFileSync(caminho_txt + "/" + arquivos_do_diretorio[i], 'UTF-8')}}
-
-/* DIVIDINDO O CONTEÚDO DO ARQUIVO EM UM VETOR DE PALAVRAS */
-arquivo_txt = arquivo_txt.split(' ')
-
-
-/* VARIÁVEL PARA CONTAR A QUANTIDADE DE VEZES QUE A PALAVRA "CLIENTE" APARECE NO ARQUIVO */
-var palavras = 0;
-
-/* FAZENDO UM FOR PARA VERIFICAR CADA PALAVRA DO ARQUIVO */
-for (let i = 0; i < arquivo_txt.length; i++) {
-
-    /* TRANSFORMANDO AS PALAVRAS EM MAIÚSCULAS PARA VER SE É IGUAL A "CLIENTE" */
-    arquivo_txt[i] = arquivo_txt[i].toUpperCase()
-    if (arquivo_txt[i] == 'CLIENTE') {
-        palavras++
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if(!paragrafo) return [];
+        return verificaPalavrasDuplicadas(paragrafo);
+    });
+    console.log(contagem);
     }
-    
+   
+    function limpaPalavras(palavra) {
+        return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    }
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+
+    listaPalavras.forEach(palavra => {
+        if (palavra.length >= 3) { 
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+        }
+    })
+    return resultado;
 }
 
-/* TRANSFORMANDO O VETOR DE PALAVRAS EM UMA STRING PARA PODER ESCREVER NO ARQUIVO */
-arquivo_txt = arquivo_txt.join(' ')
-
-
-/* ESCREVENDO O CONTEÚDO MODIFICADO NO ARQUIVO */
-fs.writeFileSync("arquivo_modificado.txt", arquivo_txt, 'UTF-8')
-
-console.log("A palavra 'CLIENTE' apareceu " + palavras + " vezes no arquivo.")
 
 /* IMPORTANDO O MODULO EXPRESS PARA CRIAR UM SERVIDOR WEB */
-const express = require('express')
+const express = require('express');
+const { findPackageJSON } = require('module');
 
 /* CRIANDO O SERVIDOR WEB */
 const app = express()
@@ -59,7 +47,7 @@ const port = 3000
 /* DEFININDO A ROTA PRINCIPAL DO SERVIDOR */
 app.get('/', (req, res) => {
     /* ENVIANDO A QUANTIDADE DE VEZES QUE A PALAVRA "CLIENTE" APARECEU NO ARQUIVO E O CONTEÚDO DO ARQUIVO MODIFICADO PARA O NAVEGADOR */
-    res.send(palavras.toString() + '\n' + '\n' + arquivo_txt)
+    res.send(link + "<br>") 
 })
 
 /* INICIANDO O SERVIDOR */
--- src/index2.js
+++ src/index2.js
@@ -21,21 +21,33 @@ function VerificarPalvrasDuplicadas(texto) {
 
 /* AJUSTA O TEXTO PARA REMOVER CARACTERES ESPECIAIS */
 function ajustarTexto(texto) {
-    const texto_ajustado = texto.map(palavra => palavra.toTitleCase().replace(/[\(\)]/g, ' ').replace(/[\n\r]+/g, ' ').replace(/\s+/g, ' ').trim());
+    const texto_ajustado = texto.flatMap(palavra => palavra.toUpperCase().replace(/[\(\)]/g, ' ').replace(/[\n\r.,]+/g, ' ').replace(/\s+/g, ' ').trim());
     return texto_ajustado;
 }
 
+
 /* SEPARA O TEXTO EM PALAVRAS */
-const texto_ajustado = ajustarTexto(fs.readFileSync(link, 'utf-8').split(' '));
+var texto_ajustado = ajustarTexto(fs.readFileSync(link, 'utf-8').split(' '));
+const contagem_palavras = VerificarPalvrasDuplicadas(texto_ajustado)
+
+
+/* LISTA COM A PALAVRAS CONTADAS E SUA QUANTIDADE */
+const texto = [] 
+for (const [chave, valor] of Object.entries(contagem_palavras))
+  texto.push(chave + " " + valor)
 
 
 
-const express = require('express')
+
+
+const express = require('express');
+const { off } = require('cluster');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send(fs.readFileSync(link, 'utf-8') + '----' + JSON.stringify(VerificarPalvrasDuplicadas(texto_ajustado)));
+  const conteudo = fs.readFileSync(link, 'utf-8') + '<br>' + texto.join('<br>'+'<br>');
+  res.send(conteudo);
 });
 
 app.listen(port, () => {
